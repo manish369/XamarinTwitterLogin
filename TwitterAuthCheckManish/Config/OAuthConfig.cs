b9a362df74a7a5aa1dc63f65684e39693b465b15
manish.pandey <manish.pandey@trigyn.com>
2019-05-20 19:39:53 +0530

    Home Page Added

---
+++ TwitterAuthCheckManish/Config/OAuthConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+namespace TwitterAuthCheckManish.Config
+{
+    public class OAuthConfig
+
+    {
+        static HomePage _HomePage;
+        static NavigationPage _Navigationpage;
+
+        public static Action SuccessFullLogin
+        { 
+            get{
+                return new Action(() =>
+                {
+                    _Navigationpage.Navigation.PushModalAsync(_HomePage);
+                });
+
+                }
+         }
+
+    }
+}
---
+++ TwitterAuthCheckManish/HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TwitterAuthCheckManish
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
